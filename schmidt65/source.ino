689b1dc2801dc55c5c38d1a9c117974d9f6dc40e
John Mettraux <jmettraux@gmail.com>
2017-05-22 11:31:10 +0900

    Add lightshow to binary dice

--- schmidt65/source.ino
+++ schmidt65/source.ino
@@ -3,18 +3,29 @@ const unsigned int LED0 = 12;
 const unsigned int LED1 = 11;
 const unsigned int LED2 = 10;
 
+void show(const long i) {
+
+  digitalWrite(LED0, i & B001);
+  digitalWrite(LED1, i & B010);
+  digitalWrite(LED2, i & B100);
+}
+
 void setup() {
 
   pinMode(LED0, OUTPUT);
   pinMode(LED1, OUTPUT);
   pinMode(LED2, OUTPUT);
 
-  randomSeed(analogRead(A0));
-  long r = random(1, 7);
+  show(1 + 2 + 4); delay(100);
+  show(0); delay(500);
+  show(1); delay(500);
+  show(2); delay(500);
+  show(4); delay(500);
+  show(1 + 2 + 4); delay(300);
 
-  digitalWrite(LED0, r & B001);
-  digitalWrite(LED1, r & B010);
-  digitalWrite(LED2, r & B100);
+  randomSeed(analogRead(A0));
+
+  show(random(1, 7));
 }
 
 void loop() {
