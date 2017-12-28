005cf92b86434a49fdd32e5bf3965c0fdcad4e16
John Mettraux <jmettraux@gmail.com>
2017-12-28 19:05:54 +0900

    Drive a single lego motor

---
+++ esp_lego_motor/README.md
@@ -0,0 +1,7 @@
+
+# nodemcu
+
+* https://github.com/nodemcu/nodemcu-devkit/wiki/Getting-Started-on-OSX
+* https://www.silabs.com/products/mcu/Pages/USBtoUARTBridgeVCPDrivers.aspx
+* https://www.silabs.com/Support%20Documents/Software/Mac_OSX_VCP_Driver.zip
+
---
+++ esp_lego_motor/source.ino
@@ -0,0 +1,103 @@
+
+//#include <ESP8266WiFi.h>
+//#include ".wifi.h"
+
+
+//WiFiServer server(80);
+
+void setup() {
+  //Serial.begin(115200);
+  //delay(10);
+
+  pinMode(D5, OUTPUT);
+  pinMode(D6, OUTPUT);
+  pinMode(D7, OUTPUT); // EN1,2
+  digitalWrite(D5, HIGH);
+  digitalWrite(D6, LOW);
+  digitalWrite(D7, HIGH);
+}
+
+//  // Connect to WiFi network
+//  Serial.println();
+//  Serial.println();
+//  Serial.print("Connecting to ");
+//  Serial.println(WIFI_SSID);
+//
+//  WiFi.begin(WIFI_SSID, WIFI_PASS);
+//
+//  while (WiFi.status() != WL_CONNECTED) {
+//    delay(500);
+//    Serial.print(".");
+//  }
+//  Serial.println("");
+//  Serial.println("WiFi connected");
+//
+//  // Start the server
+//  server.begin();
+//  Serial.println("Server started");
+//
+//  // Print the IP address
+//  Serial.print("Use this URL to connect: ");
+//  Serial.print("http://");
+//  Serial.print(WiFi.localIP());
+//  Serial.println("/");
+//}
+
+void loop() {
+
+//  // Check if a client has connected
+//  WiFiClient client = server.available();
+//  if (!client) {
+//    return;
+//  }
+//
+//  // Wait until the client sends some data
+//  Serial.println("new client");
+//  while(!client.available()){
+//    delay(1);
+//  }
+//
+//  // Read the first line of the request
+//  String request = client.readStringUntil('\r');
+//  Serial.println(request);
+//  client.flush();
+//
+//  // Match the request
+//
+//  int value = LOW;
+//  if (request.indexOf("/LED=ON") != -1)  {
+//    digitalWrite(ledPin, HIGH);
+//    value = HIGH;
+//  }
+//  if (request.indexOf("/LED=OFF") != -1)  {
+//    digitalWrite(ledPin, LOW);
+//    value = LOW;
+//  }
+//
+//// Set ledPin according to the request
+////digitalWrite(ledPin, value);
+//
+//  // Return the response
+//  client.println("HTTP/1.1 200 OK");
+//  client.println("Content-Type: text/html");
+//  client.println(""); //  do not forget this one
+//  client.println("<!DOCTYPE HTML>");
+//  client.println("<html>");
+//
+//  client.print("Led pin is now: ");
+//
+//  if(value == HIGH) {
+//    client.print("On");
+//  } else {
+//    client.print("Off");
+//  }
+//  client.println("<br><br>");
+//  client.println("<a href=\"/LED=ON\"\"><button>Turn On </button></a>");
+//  client.println("<a href=\"/LED=OFF\"\"><button>Turn Off </button></a><br />");
+//  client.println("</html>");
+//
+//  delay(1);
+//  Serial.println("Client disconnected");
+//  Serial.println("");
+}
+
