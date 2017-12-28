2205081a135aacf1f11c21fa42b779e3c9943cbb
John Mettraux <jmettraux@gmail.com>
2017-12-28 19:35:45 +0900

    Implement backward and forward web buttons

--- esp_lego_motor/source.ino
+++ esp_lego_motor/source.ino
@@ -1,13 +1,47 @@
 
-//#include <ESP8266WiFi.h>
-//#include ".wifi.h"
+#include <ESP8266WiFi.h>
+#include ".wifi.h"
 
 
-//WiFiServer server(80);
+WiFiServer server(80);
+
+void motorStop() {
+
+  digitalWrite(D5, LOW);
+  digitalWrite(D6, LOW);
+  digitalWrite(D7, LOW);
+
+  digitalWrite(D0, LOW);
+  digitalWrite(D1, LOW);
+  digitalWrite(D2, LOW);
+}
+
+void motorForward() {
+
+  digitalWrite(D5, HIGH);
+  digitalWrite(D6, LOW);
+  digitalWrite(D7, HIGH);
+
+  digitalWrite(D0, LOW);
+  digitalWrite(D1, HIGH);
+  digitalWrite(D2, HIGH);
+}
+
+void motorBackward() {
+
+  digitalWrite(D5, LOW);
+  digitalWrite(D6, HIGH);
+  digitalWrite(D7, HIGH);
+
+  digitalWrite(D0, HIGH);
+  digitalWrite(D1, LOW);
+  digitalWrite(D2, HIGH);
+}
 
 void setup() {
-  //Serial.begin(115200);
-  //delay(10);
+
+  Serial.begin(115200);
+  delay(10);
 
   pinMode(D5, OUTPUT);
   pinMode(D6, OUTPUT);
@@ -17,96 +51,74 @@ void setup() {
   pinMode(D1, OUTPUT);
   pinMode(D2, OUTPUT); // EN3,4
 
-  digitalWrite(D5, HIGH);
-  digitalWrite(D6, LOW);
-  digitalWrite(D7, HIGH);
+  motorStop();
 
-  digitalWrite(D0, HIGH);
-  digitalWrite(D1, LOW);
-  digitalWrite(D2, HIGH);
+  // Connect to WiFi network
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(WIFI_SSID);
+
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+
+  // Start the server
+  server.begin();
+  Serial.println("Server started");
+
+  // Print the IP address
+  Serial.print("Use this URL to connect: ");
+  Serial.print("http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("/");
 }
-
-//  // Connect to WiFi network
-//  Serial.println();
-//  Serial.println();
-//  Serial.print("Connecting to ");
-//  Serial.println(WIFI_SSID);
-//
-//  WiFi.begin(WIFI_SSID, WIFI_PASS);
-//
-//  while (WiFi.status() != WL_CONNECTED) {
-//    delay(500);
-//    Serial.print(".");
-//  }
-//  Serial.println("");
-//  Serial.println("WiFi connected");
-//
-//  // Start the server
-//  server.begin();
-//  Serial.println("Server started");
-//
-//  // Print the IP address
-//  Serial.print("Use this URL to connect: ");
-//  Serial.print("http://");
-//  Serial.print(WiFi.localIP());
-//  Serial.println("/");
-//}
 
 void loop() {
 
-//  // Check if a client has connected
-//  WiFiClient client = server.available();
-//  if (!client) {
-//    return;
-//  }
-//
-//  // Wait until the client sends some data
-//  Serial.println("new client");
-//  while(!client.available()){
-//    delay(1);
-//  }
-//
-//  // Read the first line of the request
-//  String request = client.readStringUntil('\r');
-//  Serial.println(request);
-//  client.flush();
-//
-//  // Match the request
-//
-//  int value = LOW;
-//  if (request.indexOf("/LED=ON") != -1)  {
-//    digitalWrite(ledPin, HIGH);
-//    value = HIGH;
-//  }
-//  if (request.indexOf("/LED=OFF") != -1)  {
-//    digitalWrite(ledPin, LOW);
-//    value = LOW;
-//  }
-//
-//// Set ledPin according to the request
-////digitalWrite(ledPin, value);
-//
-//  // Return the response
-//  client.println("HTTP/1.1 200 OK");
-//  client.println("Content-Type: text/html");
-//  client.println(""); //  do not forget this one
-//  client.println("<!DOCTYPE HTML>");
-//  client.println("<html>");
-//
-//  client.print("Led pin is now: ");
-//
-//  if(value == HIGH) {
-//    client.print("On");
-//  } else {
-//    client.print("Off");
-//  }
-//  client.println("<br><br>");
-//  client.println("<a href=\"/LED=ON\"\"><button>Turn On </button></a>");
-//  client.println("<a href=\"/LED=OFF\"\"><button>Turn Off </button></a><br />");
-//  client.println("</html>");
-//
-//  delay(1);
-//  Serial.println("Client disconnected");
-//  Serial.println("");
+  // Check if a client has connected
+
+  WiFiClient client = server.available();
+  if ( ! client) return;
+
+  // Wait until the client sends some data
+
+  Serial.println("new client");
+  while ( ! client.available()) delay(1);
+
+  // Read the first line of the request
+
+  String request = client.readStringUntil('\r');
+  Serial.println(request);
+  client.flush();
+
+  // Match the request
+
+  if (request.indexOf("/FORWARD") != -1) motorForward();
+  else if (request.indexOf("/BACKWARD") != -1) motorBackward();
+  delay(1000);
+  motorStop();
+
+  // Return the response
+
+  client.println("HTTP/1.1 200 OK");
+  client.println("Content-Type: text/html");
+  client.println(""); //  do not forget this one
+  client.println("<!DOCTYPE HTML>");
+  client.println("<html>");
+
+  client.println("<br><br>");
+  client.println("<a href=\"/FORWARD\"\"><button>FORWARD</button></a>");
+  client.println("<a href=\"/BACKWARD\"\"><button>BACKWARD</button></a><br />");
+  client.println("</html>");
+
+  delay(1);
+  Serial.println("Client disconnected");
+  Serial.println("");
 }
 
