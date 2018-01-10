0ebd703c855f2e9f4ba1279ab0bdad087318f019
John Mettraux <jmettraux@gmail.com>
2018-01-10 11:45:20 +0900

    Clean up esp dht source

--- esp_dht22/source.ino
+++ esp_dht22/source.ino
@@ -12,6 +12,8 @@
 
 #define SERVER "http://10.0.1.6:8080/"
 
+#define DEEP_SLEEP_TIME 60 // seconds
+
 DHT dht(D3, DHTTYPE);
 
 
@@ -59,94 +61,27 @@ void setup() {
   Serial.print(" connected as ");
   Serial.println(WiFi.localIP());
 
-  // POST data
-
-  char uri[512];
-  snprintf(uri, 512, "%s/?h=%.2f&t=%.2f&i=%.2f", SERVER, h, t, i);
-  Serial.println(uri);
+  // well, GET data
 
   HTTPClient http;
-  http.begin(SERVER);
+
+  String uri = SERVER;
+  uri += "?h="; uri += h;
+  uri += "&t="; uri += t;
+  uri += "&i="; uri += i;
+  uri += "&m="; uri += WiFi.macAddress();
+  Serial.println(uri);
+
+  http.begin(uri);
   //http.addHeader("Content-Type", "application/json");
 
   int httpCode = http.GET();
-//  client.print("{");
-//  client.print("\"humidity\":"); client.print(h); client.print(",");
-//  client.print("\"temperature\":"); client.print(t); client.print(",");
-//  client.print("\"heatIndex\":"); client.print(i); client.print(",");
-//  client.print("\"mac\":\""); client.print(WiFi.macAddress()); client.print("\"");
-//  client.print("}");
 
   // Sleep deep
 
-  ESP.deepSleep(5e6); // five seconds
-
-//  // Start the server
-//
-//  server.begin();
-//  Serial.println("Server started");
-//
-//  // Print the IP address
-//
-//  Serial.println();
-//  Serial.println(WiFi.macAddress());
-//  Serial.print("http://");
-//  Serial.print(WiFi.localIP());
-//  Serial.println("/");
+  ESP.deepSleep(DEEP_SLEEP_TIME * 1e6);
 }
 
 void loop() {
-
-//  // Check if a client has connected
-//
-//  WiFiClient client = server.available();
-//  if ( ! client) return;
-//
-//  // Wait until the client sends some data
-//
-//  //Serial.println("new client");
-//  while ( ! client.available()) delay(1);
-//
-//  // Display request
-//
-//  String request = client.readStringUntil('\r');
-//
-//  // https://github.com/esp8266/Arduino/blob/master/libraries/ESP8266WiFi/src/WiFiClient.h
-//
-//  Serial.print(request); Serial.print(" from ");
-//  Serial.print(client.remoteIP()); Serial.print(":");
-//  Serial.print(client.remotePort());
-//
-//  client.flush();
-//
-//  // Match the request
-//
-////  if (request.indexOf("/FORWARD") != -1) motorForward();
-////  else if (request.indexOf("/BACKWARD") != -1) motorBackward();
-////  else if (request.indexOf("/LEFT") != -1) motorLeft();
-////  else if (request.indexOf("/RIGHT") != -1) motorRight();
-//
-//  // read sensor
-//
-//  float h = dht.readHumidity();
-//  float t = dht.readTemperature();
-//  float i = dht.computeHeatIndex(t, h, false);
-//  Serial.print(" -- ");
-//  Serial.print(h); Serial.print(" % / ");
-//  Serial.print(t); Serial.print(" C / ");
-//  Serial.print(i); Serial.print(" C");
-//  Serial.println();
-//
-//  // Return the response
-//
-//  client.println("HTTP/1.1 200 OK");
-//  client.println("Content-Type: application/json");
-//  client.println(""); //  do not forget this one
-//  client.print("{");
-//  client.print("\"humidity\":"); client.print(h); client.print(",");
-//  client.print("\"temperature\":"); client.print(t); client.print(",");
-//  client.print("\"heatIndex\":"); client.print(i); client.print(",");
-//  client.print("\"mac\":\""); client.print(WiFi.macAddress()); client.print("\"");
-//  client.print("}");
 }
 
